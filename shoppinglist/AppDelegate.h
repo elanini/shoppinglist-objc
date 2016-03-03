7952a0ea39e595878cea05b727b81ea6e16ca7ff
elanini <elanini@ucsc.edu>
2016-03-03 14:41:53 -0800

    Initial Commit

---
+++ shoppinglist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shoppinglist
+//
+//  Created by Eric Lanini on 3/3/16.
+//  Copyright © 2016 Eric Lanini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shoppinglist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shoppinglist
+//
+//  Created by Eric Lanini on 3/3/16.
+//  Copyright © 2016 Eric Lanini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
